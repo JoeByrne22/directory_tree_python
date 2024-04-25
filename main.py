fff6fc6d6982112d5ffe25d72edd9d9325cc4305
joe.byrne1 <joe.byrne@and.digital>
2024-04-25 14:56:28 +0100

    main checker

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from generate_tree import generate_tree
 
 if __name__ == "__main__":
-    root_directory = input("Enter the path of the root directory: ")
+    root_directory = input("*Enter the path of the root directory: ")
     generate_tree(root_directory)
